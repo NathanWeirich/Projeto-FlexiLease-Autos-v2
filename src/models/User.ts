4fc12e558b10674a690dc89bd282164b9804c3b7
Nathan Weirich <nathanweirich@gmail.com>
2024-07-03 15:10:51 -0300

    feat(users): add model

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    cpf: { type: String, required: true, unique: true },
+    birth: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    cep: { type: String, required: true },
+    qualified: { type: String, required: true },
+    patio: { type: String, required: true, default: "N/A" },
+    complement: { type: String, required: true, default: "N/A" },
+    neighborhood: { type: String, required: true, default: "N/A" },
+    locality: { type: String, required: true, default: "N/A" },
+    uf: { type: String, required: true, default: "N/A" },
+  },
+  {
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.__v;
+      },
+    },
+  },
+);
+
+const User = model("User", UserSchema);
+export default User;
