6652fd9a27d7837c8375f0a1b71b565cec5609c4
Nathan Weirich <nathanweirich@gmail.com>
2024-07-03 16:51:20 -0300

    feat(users): add user validation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
 import UserController from "../controllers/user.controller";
 import { UserService } from "../services/UserService";
+import { validateUser } from "../validation/userValidation";
 
 const router = Router();
 const userService = new UserService();
 const userController = new UserController(userService);
 
-router.post("/user", userController.registerUser.bind(userController));
+router.post(
+  "/user",
+  validateUser,
+  userController.registerUser.bind(userController),
+);
 router.get("/user", userController.getAllUsers.bind(userController));
 router.get("/user/:id", userController.getUserById.bind(userController));
-router.put("/user/:id", userController.updateUser.bind(userController));
+router.put(
+  "/user/:id",
+  validateUser,
+  userController.updateUser.bind(userController),
+);
 router.delete("/user/:id", userController.deleteUser.bind(userController));
 router.post(
   "/authenticate",
